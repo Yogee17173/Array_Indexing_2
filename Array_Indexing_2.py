c79db67f80b92e2bb49c90dc014afea7136a4d81
YOGEESH K P <yogeesh793@gmail.com>
2025-03-06 16:07:43 +0530

    Add files via upload

---
+++ Array_Indexing_2.py
@@ -0,0 +1,9 @@
+# Get the second element from the following array.
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr[1])
+
+#1
+import numpy as yp
+yrr=yp.array([1,2,3,4,5])
+print(yrr[1])
